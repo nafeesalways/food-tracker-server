e258d872a905c3f0de1cca428e0c6597a34463e0
nafeesalways <nafeezmy@gmail.com>
2025-08-09 22:38:16 +0600

    server console done

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Food Tracker is Testing");
+  res.send("Food Tracker is Running");
 });
 
 app.listen(port, () => {
